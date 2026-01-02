bb05949b40f66ea54b72c3dff461292a40b19f6f
Alex Sindalovsky <design@acedzn.com>
2026-01-03 01:43:12 +0200

    more tries

--- packages/db/src/provider.tsx
+++ packages/db/src/provider.tsx
@@ -8,8 +8,12 @@ import { ClerkProvider, useAuth } from "@clerk/nextjs";
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL as string);
 
 export function SharedAuthProvider({ children }: { children: ReactNode }) {
+    const domain = process.env.NEXT_PUBLIC_CLERK_COOKIE_DOMAIN ||
+        (process.env.NODE_ENV === "production" ? "acedzn.dev" : undefined);
+
     return (
-        <ClerkProvider>
+        // @ts-expect-error - domain triggering satellite types but we are using it for cookie hoisting
+        <ClerkProvider domain={domain} isSatellite={false}>
             <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
                 {children}
             </ConvexProviderWithClerk>
