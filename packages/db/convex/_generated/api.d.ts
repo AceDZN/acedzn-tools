03b9645746c7507287e5718c78a6ea9eace62302
Alex Sindalovsky <design@acedzn.com>
2026-01-03 03:09:44 +0200

    added users type

--- packages/db/convex/_generated/api.d.ts
+++ packages/db/convex/_generated/api.d.ts
@@ -9,6 +9,7 @@
  */
 
 import type * as dictation from "../dictation.js";
+import type * as users from "../users.js";
 
 import type {
   ApiFromModules,
@@ -18,6 +19,7 @@ import type {
 
 declare const fullApi: ApiFromModules<{
   dictation: typeof dictation;
+  users: typeof users;
 }>;
 
 /**
