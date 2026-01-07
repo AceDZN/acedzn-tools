646af94c6868d3a0aacaab1f8c3d938b4f32d55c
Alex Sindalovsky <design@acedzn.com>
2026-01-07 21:55:07 +0200

    proxy fix

--- apps/dictation/proxy.ts
+++ apps/dictation/proxy.ts
@@ -63,5 +63,7 @@ export const config = {
         '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest|glb|gltf|mp3|wav)).*)',
         // Always run for API routes
         '/(api|trpc)(.*)',
+        // Always run for ingest routes
+        '/ingest(.*)',
     ],
 };
