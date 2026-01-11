de20b6d5397d6cd9753b138e7298d3e058d93c91
Alex Sindalovsky <design@acedzn.com>
2026-01-11 16:35:47 +0200

    more seo.

--- apps/workbook/lib/constants/index.ts
+++ apps/workbook/lib/constants/index.ts
@@ -1,6 +1,21 @@
 export * from "./global";
-export * from "./science_chapter1";
-export * from "./science_chapter2";
-export * from "./science_chapter3";
-export * from "./science_chapter4";
+
+// Chapter questions are imported dynamically in pages via:
+// import(`@/lib/constants/${subjectId}_${chapterId}`)
+// Re-exporting individual question arrays for direct use when needed
+export {
+  CHAPTER_QUESTIONS as CHAPTER1_QUESTIONS,
+} from "./science_chapter1";
+
+export {
+  CHAPTER_QUESTIONS as CHAPTER2_QUESTIONS,
+} from "./science_chapter2";
+
+export {
+  CHAPTER_QUESTIONS as CHAPTER3_QUESTIONS,
+} from "./science_chapter3";
+
+export {
+  CHAPTER_QUESTIONS as CHAPTER4_QUESTIONS,
+} from "./science_chapter4";
 
